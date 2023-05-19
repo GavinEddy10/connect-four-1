a0a63938cdf2d3aaee4c77e495e5d854777a8a4f
gavin <geddy2023@damien-hs.edu>
2023-05-19 14:01:21 -0700

    Horizontal Works

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -13,83 +13,129 @@ export const Board = () => {
         ['', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '']
     ]);
-
+    const COLUMNS = 7;
+    const ROWS = 6;
     const [currPlayer, setCurrPlayer] = useState('X');
     const [oppPlayer, setOppPlayer] = useState('O');
     const [gameOver, setGameOver] = useState(false);
 
 
+    
+    const validCol = (col) => {
+        return col >= 0 && col < COLUMNS;
+    }
+    const validRow = (row) => {
+        return row >= 0 && row < ROWS;
+    }
+
+    const checkWinVert = (row,column,ch) => {
+        if (validRow(row+1) && board[row + 1][column] === ch) {
+            if (validRow(row+2) && board[row + 2][column] === ch) {
+                if (validRow(row+3) && board[row + 3][column] === ch) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    const checkWinDLtR = (row, column, ch) => {
+        //all diagonal solutions upper left to bot right
+       
+        if (validRow(row+1) && validCol(column+1) && board[row + 1][column + 1] === ch) {
+            if (validRow(row+2) && validCol(column+2) && board[row + 2][column + 2] === ch) {
+                if (validRow(row+3) && validCol(column+3) && board[row + 3][column + 3] === ch) {
+                    return true;
+                }
+            }
+        }
+    
+        if (validRow(row-1) && validCol(column-1) && board[row - 1][column - 1] === ch) {
+            if (validRow(row+1) && validCol(column+1) && board[row + 1][column + 1] === ch) {
+                if (validRow(row+2) && validCol(column+2) && board[row + 2][column + 2] === ch) {
+                    return true;
+                }
+            }
+        }
+    
+        if (validRow(row-2) && validCol(column-2) && board[row - 2][column - 2] === ch) {
+            if (validRow(row-1) && validCol(column-1) && board[row - 1][column - 1] === ch) {
+                if (validRow(row+1) && validCol(column+1) && board[row + 1][column + 1] === ch) {
+                    return true;
+                }
+            }
+        }
+    
+        if (validRow(row-2) && validCol(column-2) && board[row - 2][column - 2] === ch) {
+            if (validRow(row-1) && validCol(column-1) && board[row - 1][column - 1] === ch) {
+                if (validRow(row-3) && validCol(column-3) && board[row - 3][column - 3] === ch) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    const checkWinDBLtR = (row, column, ch) => {
+        //all diagonal from bot let to upper right
+        
+        if (validRow(row+1) && validCol(column-1) && board[row + 1][column - 1] === ch) {
+            if (validRow(row+2) && validCol(column-2) && board[row + 2][column - 2] === ch) {
+                if (validRow(row+3) && validCol(column-3) && board[row + 3][column - 3] === ch) {
+                    return true;
+                }
+            }
+        }
+   
+        if (validRow(row-1) && validCol(column+1) && board[row - 1][column + 1] === ch) {
+            if (validRow(row+1) && validCol(column-1) && board[row + 1][column - 1] === ch) {
+                if (validRow(row+2) && validCol(column-2) && board[row + 2][column - 2] === ch) {
+                    return true;
+                }
+            }
+        }
+  
+    
+        if (validRow(row+1) && validCol(column-1) && board[row + 1][column - 1] === ch) {
+            if (validRow(row-1) && validCol(column+1) && board[row - 1][column + 1] === ch) {
+                if (validRow(row-2) && validCol(column+2) && board[row - 2][column + 2] === ch) {
+                    return true;
+                }
+            }
+        }
+    
+    
+        if (validRow(row-1) && validCol(column+1) && board[row - 1][column + 1] === ch) {
+            if (validRow(row-2) && validCol(column+2) && board[row - 2][column + 2] === ch) {
+                if (validRow(row-3) && validCol(column+3) && board[row - 3][column + 3] === ch) {
+                    return true;
+                }
+            }
+        }
+    
+    }
+
+    const checkWinHorizontal = (row, column, ch) => {//finds left most point, then checks all four
+        //all horizontal possibilities
+        board[row][column] = ch;//important
+        for (var i = 0; i < 4; i++) {//gets farthest left spot
+            if (validCol(column-1) &&  board[row][column-1] === ch)
+                column -= 1;
+            else 
+                break;
+        }
+        if (currentSpot === board[row][column+1] && board[row][column+1] === board[row][column+2] && board[row][column+2] === board[row][column+3])
+            return true;
+        return false;
+       
+    }
+
+
     const checkWin = (row, column, ch) => {
-        // EXERCISE: This function does not cover all possible winning combinations. Edit the code to cover all possibilities. A working solution in C# (don't worry--if you know JavaScript you should understand most of it) exists at https://dotnetfiddle.net/FZGpbS Line #128
-        try {
-            if (board[row + 1][column] === ch) {
-                if (board[row + 2][column] === ch) {
-                    if (board[row + 3][column] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
-
-        try {
-            if (board[row + 1][column + 1] === ch) {
-                if (board[row + 2][column + 2] === ch) {
-                    if (board[row + 3][column + 3] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
-
-        try {
-            if (board[row + 1][column - 1] === ch) {
-                if (board[row + 2][column - 2] === ch) {
-                    if (board[row + 3][column - 3] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
-
-        try {
-            if (board[row][column + 1] === ch) {
-                if (board[row][column + 2] === ch) {
-                    if (board[row][column + 3] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
-
-        try {
-            if (board[row][column - 1] === ch) {
-                if (board[row][column - 2] === ch) {
-                    if (board[row][column - 3] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
-
-        try {
-            if (board[row - 1][column - 1] === ch) {
-                if (board[row - 2][column - 2] === ch) {
-                    if (board[row - 3][column - 3] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
-
-        try {
-            if (board[row - 1][column + 1] === ch) {
-                if (board[row - 2][column + 2] === ch) {
-                    if (board[row - 3][column + 3] === ch) {
-                        return true;
-                    }
-                }
-            }
-        } catch (e) { console.log(e) }
+        return (
+            checkWinDBLtR(row, column, ch) ||
+            checkWinDLtR(row, column, ch) ||
+            checkWinHorizontal(row, column, ch) ||
+            checkWinVert(row, column, ch)
+        )
     };
 
     const updateBoard = (row, column, ch) => {
@@ -102,6 +148,7 @@ export const Board = () => {
     };
 
 
+
     const handleClick = (e) => {
         const column = e.target.getAttribute('x');
         let row = board.findIndex((rowArr, index) => {
@@ -111,6 +158,7 @@ export const Board = () => {
         // Only go up one row if the slot is NOT at the bottom
         if (row !== (board.length - 1)) row -= 1;
         if (board[row][column] !== '') row -= 1;
+
 
 
 
@@ -127,16 +175,18 @@ export const Board = () => {
     };
 
 
+
+    //<button id='newGame' onClick={gameOver ? makeNewGame(): null}> New Game </button>
     return (
         <>
             {gameOver && (
-                <h1>Game Over! {oppPlayer == 'X' ? 'Red' : 'Black'} Wins!</h1>
+                <h1>Game Over! {oppPlayer === 'X' ? 'Red' : 'Black'} Wins!</h1>
             )}
             <h2 id='playerDisplay'>{currPlayer === 'X' ? 'Red' : 'Black'} Move</h2>
+            
             <div id='board'
                 onClick={gameOver ? null : handleClick}
             >
-
                 {board.map((row, i) => {
                     return row.map((ch, j) => <Slot ch={ch} y={i} x={j} />);
                 })}
